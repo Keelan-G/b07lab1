36a492e2bd5b6500bf44f6ea780d8c6ccf541aaf
Keelan-G <129237066+Keelan-G@users.noreply.github.com>
2023-10-02 23:59:51 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -2,10 +2,10 @@ public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
+		double [] c1 = {6,5};
 		int [] e1 = {0,3};
 		Polynomial p1 = new Polynomial(c1, e1);
-		double [] c2 = {0,-2,0,0,-9};
+		double [] c2 = {-2,-9};
 		int [] e2 = {1,4};
 		Polynomial p2 = new Polynomial(c2, e2);
 		Polynomial s = p1.add(p2);
